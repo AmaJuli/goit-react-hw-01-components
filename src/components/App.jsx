82188183f6625d8a47e9632b023222b9528de6cb
AmaJuli <ostapchukyuliiaa@gmail.com>
2024-09-08 18:12:10 +0300

    Опис змін, які ви зробили

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,27 @@
-import {Profiles} from 'profile/profile';
-import user from '../datajson/user.json'
-import data from '../datajson/data.json'
-import friends from '../datajson/friends.json';
-import transactions from '../datajson/transactions.json'
-import { Statistics } from 'statistics/statistics';
-import { FriendList } from 'friends/friends';
-import { TransactionHistory } from 'transaction/transaction';
-export const App = () => {
-  return (
-<>
-<Profiles
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
-<Statistics title="Upload stats" stats={data} />
-<FriendList friends={friends} />
-<TransactionHistory items={transactions}/>
-</>
-    
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+export function App() {
+  document.title = 'Homework 1';
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
-};
+}
